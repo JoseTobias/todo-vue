137629bfabdc60ae9791c75d407dbd2de01bc636
JoseTobias <tobiassouza0@gmail.com>
2020-02-01 08:56:31 -0300

    delete finished

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -16,7 +16,7 @@
           <button class='btn btn-warning' v-if="todo.done" @click="marked(todo, false)">
             <i class='fas fa-undo'></i>
           </button>
-          <button class='btn btn-danger' v-if="todo.done">
+          <button class='btn btn-danger' v-if="todo.done" @click="remove(todo)">
             <i class='fas fa-trash'></i>
           </button>
         </td>
@@ -44,6 +44,10 @@ export default {
         method: "PUT",
         headers: {"Content-Type": "application/x-www-form-urlencoded"},
         body: new URLSearchParams({...todo, done: choice})})
+      .then(() => this.refresh())
+    },
+    remove (todo) {
+      fetch('http://localhost:3003/api/todos/' + todo._id, {method: "DELETE"})
       .then(() => this.refresh())
     }
   },
