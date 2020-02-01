249a4dbcc26af99517d22aaac2b9d1f03b9bd5cf
JoseTobias <tobiassouza0@gmail.com>
2020-02-01 11:53:30 -0300

    add event key

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -3,6 +3,9 @@
         <div class='col-xs-12 col-sm-9 col-md-10'>
             <input id='description' class='form-control'
             placeholder='Cadastre uma tarefa'
+            @keyup.enter.exact="add()"
+            @keyup.shift.enter="search()"
+            @keyup.esc.exact="clear()"
             v-model="description" />
         </div>
         <div class='col-xs-12 col-sm-3 col-md-2'>
